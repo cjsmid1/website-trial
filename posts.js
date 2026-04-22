119826d828ee7c5d5e3f052705ed7cb18cd5f091
cjsmid1 <cjsmid@hotmail.co.uk>
2026-04-22 13:43:03 +0100

    First proper post!

--- posts.js
+++ posts.js
@@ -1,4 +1,61 @@
 const posts = [
+{
+    id: "mushroom-wellington",
+    title: "🍄 Mushroom Wellington",
+    category: "Recipe",
+    tags: ["vegetarian", "dinner-party"],
+    excerpt: "The time I took a beef wellington, turned it vegetarian and sent it off to asia...",
+    content: `
+      <img src="images/mushroom-wellington-puff.jpg" alt="Nice view">
+	  <p><b>TLDR:</b> Great pastry centre piece for a roast, I fed 7 people and had left over filling for sandwiches so feel free to half the recipe if desired. Be sure to leave enough time as the recipe includes some cooling steps and 20 minutes in fridge before final bake!</p>
+	  <h2>Ingredients</h2>
+	  <p class="ingredient-section">Filling:</p>
+      <ul>
+		<li>2 tbsp Sesame Oil</li>
+		<li>500g Mushrooms (finely chopped)</li>
+		<li>6 cloves Garlic, minced</li>
+		<li>1 tbsp Ginger, grated or minced</li>
+		<li>6 Spring Onions, finely sliced</li>
+		<li>(Opt) 2 tbsp Sherry, White Wine or Rice Wine</li>
+        <li>300g Vegetarian Shredded “Duck” (Linda McCartney Vegetarian Shredded Hoisin Duck is my favourite)</li>
+        <li>1 tin Water Chestnuts (drained + finely chopped)</li>
+		<li>2 tbsp Soy Sauce</li>
+		<li>2 tbsp Hoisin Sauce</li>
+		<li>2 tsp Sesame Seeds</li>
+	  </ul>
+		<p class="ingredient-section">Structure:</p>
+	  <ul>	
+		<li>3 sheets Rice Paper (can do more if you want to double them)</li>
+		<li>1–2 tsp Sesame Oil (for brushing)</li>
+		<li>2 sheets Ready-rolled Puff Pastry</li>
+		<li>1 egg or plant milk (for glazing)</li>
+      </ul>
+      <h2>Instructions</h2>
+      <ol class="instructions">
+        <li class="section-title">Make the Filling:</li>
+		<li>Pan fry mushrooms in the sesame oil with pinch of salt. Stir occassionally and cook till water completely cooked off and browned (expect 15-20 mins)</li>
+		<li>Add garlic, ginger & spring onions, mix & cook for couple minutes</li>
+		<li>Add vegetarian duck, water chestnuts, soy sause & hoisin. Cook until hot & liquid all gone (we want the filling as dry as possible!)</li>
+        <li>Mix in sesame seeds, taste & adjust as desired, then leave to cool</li>
+        <li class="section-title">Make the Rice Paper Barrier (the anti-sog protection):</li>
+		<li>Briefly dip each piece of rice paper in water and lay flat to let soften for ~30 seconds</li>
+		<li>Lightly overlap 3 sheets to form a wide base & brush with sesame oil (I did this on top of baking paper to have extra control and reduce sticking)</li>
+		<li>Pile filling onto rice paper and form a tight log, gently wrap the edges of the rice paper over the top to form a giant sausage shape (my rice paper broke a little but final wellington was still great so don't panic!)</li>
+		<li>(Optional) cook in oven at 180°C for 10 minutes to improve the rice paper texture and let cool</li>
+		<figure class="post-image">
+		  <img src="images/mushroom-wellington-prepuff.jpg" alt="Mushroom Wellington wrapped in rice paper coating">
+		  <figcaption>If it looks anything like this, you're doing great! 🙌</figcaption>
+        </figure>
+		<li class="section-title">Final Assembly and Bake:</li>
+		<li>Roll out the puff pastry, sealing the 2 sheets together with your fingers or a rolling pin</li>
+		<li>Place the cooled log in the centre and wrap the pastry around it (decorate the top with any remaining pastry however you like, or place seam down for minimalist look)</li>
+		<li>Chill in fridge for 20 minutes</li>
+		<li>Brush with egg wash or milk & bake 35 minutes at 180°C fan till deep golden</li>
+      </ol>
+	  <p style="text-align: center;">✦ ────  ^_^  ──── ✦</p>
+	  <p>Enjoy! I served mine with a honey onion gravy, though if you want to fully embrace the asian influence you can also use a soy & rice vinegar dip, or just more hoisin sauce!</p>
+    `
+  },
   {
     id: "mushroom-burgers",
     title: "🍔 Mushroom Burgers",
